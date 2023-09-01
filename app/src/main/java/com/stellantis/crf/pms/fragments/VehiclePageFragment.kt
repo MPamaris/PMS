dd902e29cd2efa547681e67f4eca545daa4e06a0
SD1592 <alessio.collura@external.stellantis.com>
2023-09-01 17:09:01 +0200

    Added functions on vehicle page

--- app/src/main/java/com/stellantis/crf/pms/fragments/VehiclePageFragment.kt
+++ app/src/main/java/com/stellantis/crf/pms/fragments/VehiclePageFragment.kt
@@ -1,15 +1,23 @@
 package com.stellantis.crf.pms.fragments
 
+import android.annotation.SuppressLint
+import android.content.ContentValues
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.Toast
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import com.stellantis.crf.pms.PmsRepository
 import com.stellantis.crf.pms.R
 import com.stellantis.crf.pms.databinding.FragmentVehiclePageBinding
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+import java.util.Locale
 
 class VehiclePageFragment : Fragment() {
 
@@ -22,6 +30,10 @@ class VehiclePageFragment : Fragment() {
 
         binding = FragmentVehiclePageBinding.inflate(layoutInflater)
         val view = binding.root
+
+        isNotification()
+        getUser()
+        getVehicleOwned()
 
         binding.bottomNavigation.setOnItemSelectedListener { item ->
             when (item.itemId) {
@@ -53,6 +65,104 @@ class VehiclePageFragment : Fragment() {
         }
 
         return view
+    }
+
+    private fun isNotification() {
+
+        activity?.let { PmsRepository.initialize(it) }
+        lifecycleScope.launch {
+            try {
+                coroutineScope {
+                    launch {
+                        PmsRepository.getNotification()?.let { notificationNew ->
+
+                            val notification =
+                                notificationNew.notifications?.get(0)?.notificationSeverity
+
+                            when (notification) {
+                                "0" -> binding.idIncludeTop.tileUserAndNotifications.idBadgeNotification.visibility =
+                                    View.VISIBLE
+
+                                "1" -> binding.idIncludeTop.tileUserAndNotifications.idBadgeNotification.visibility =
+                                    View.INVISIBLE
+
+                            }
+
+                        } ?: throw Exception("discovery null response")
+                    }
+                }
+            } catch (e: Exception) {
+                Toast.makeText(activity, "Network Error", Toast.LENGTH_LONG).show()
+                // Discovery error
+                Log.e(ContentValues.TAG, "callDiscovery(): Error: $e")
+            }
+        }
+    }
+    @SuppressLint("SetTextI18n")
+    private fun getUser() {
+
+        activity?.let { PmsRepository.initialize(it) }
+        lifecycleScope.launch {
+            try {
+                coroutineScope {
+                    launch {
+                        PmsRepository.getUserInfo()?.let { userInfoNew ->
+                            val name = userInfoNew.name
+                            val surname = userInfoNew.surname
+                            val initialName =
+                                userInfoNew.name?.get(0).toString().toUpperCase(Locale.ROOT)
+                            val initialSurname =
+                                userInfoNew.surname?.get(0).toString().toUpperCase(Locale.ROOT)
+
+                            binding.idIncludeTop.tileUserAndNotifications.idTexvViewOutInitials.text =
+                                initialName + initialSurname
+
+                        } ?: throw Exception("discovery null response")
+                    }
+                }
+            } catch (e: Exception) {
+                Toast.makeText(activity, "Network Error", Toast.LENGTH_LONG).show()
+                // Discovery error
+                Log.e(ContentValues.TAG, "callDiscovery(): Error: $e")
+            }
+        }
+    }
+    private fun getVehicleOwned() {
+
+        activity?.let { PmsRepository.initialize(it) }
+        lifecycleScope.launch {
+            try {
+                coroutineScope {
+                    launch {
+                        PmsRepository.getVehicleOwned()?.let { vehicleInfoNew ->
+
+                            val model = vehicleInfoNew.vehicles?.map { it.model }
+
+                            val str = model.toString().replace("[", " ").replace("]", "")
+                            val delimiter = ","
+                            val partsOfListOfVehicles = str.split(delimiter)
+
+                            for (i in partsOfListOfVehicles.indices) {
+                                val spinner =
+                                    binding.idIncludeTop.idTileChoiceVehicle.idChooseVehicleSpinner
+                                val dataAdapter: ArrayAdapter<String>? = activity?.let {
+                                    ArrayAdapter<String>(
+                                        it,
+                                        android.R.layout.simple_spinner_item, partsOfListOfVehicles
+                                    )
+                                }
+                                spinner.adapter = dataAdapter
+                            }
+
+                        } ?: throw Exception("discovery null response")
+                    }
+                }
+            } catch (e: Exception) {
+                Toast.makeText(activity, "Network Error", Toast.LENGTH_LONG).show()
+                // Discovery error
+                Log.e(ContentValues.TAG, "callDiscovery(): Error: $e")
+            }
+        }
     }
 
     private fun goToHomePage() {
