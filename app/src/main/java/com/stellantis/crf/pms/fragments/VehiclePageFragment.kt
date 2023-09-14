3f348da9f7c8787f6ab9d4bd1a50fe378d24020d
SD1592 <alessio.collura@external.stellantis.com>
2023-09-14 16:40:23 +0200

    Fixed toolbar on vehicle page

--- app/src/main/java/com/stellantis/crf/pms/fragments/VehiclePageFragment.kt
+++ app/src/main/java/com/stellantis/crf/pms/fragments/VehiclePageFragment.kt
@@ -2,17 +2,21 @@ package com.stellantis.crf.pms.fragments
 
 import android.annotation.SuppressLint
 import android.content.ContentValues
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.stellantis.crf.pms.PmsRepository
+import com.stellantis.crf.pms.R
 import com.stellantis.crf.pms.databinding.FragmentVehiclePageBinding
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
@@ -31,7 +35,37 @@ class VehiclePageFragment : Fragment() {
         binding = FragmentVehiclePageBinding.inflate(layoutInflater)
         val view = binding.root
 
-        isNotification()
+        binding.idIncludeTop.idTileChoiceVehicle.idChooseVehicleSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                val currentVehicle = binding.idIncludeTop.idTileChoiceVehicle.idChooseVehicleSpinner.selectedItem.toString()
+
+                val Choice: Int = binding.idIncludeTop.idTileChoiceVehicle.idChooseVehicleSpinner.selectedItemPosition
+                val sharedPrefS: SharedPreferences = activity!!.getSharedPreferences("FileName", 0)
+                val prefEditor = sharedPrefS.edit()
+                prefEditor.putInt("spinnerChoice", Choice)
+                prefEditor.commit()
+                //Toast.makeText(activity, "Saved", Toast.LENGTH_SHORT).show()
+
+                if (currentVehicle.contains("Renegade")) {
+                    getRenegadeInfo()
+                    //binding.idTvCheckVehicle.text = "Renegade"
+                    //Toast.makeText(activity, "" + currentVehicle, Toast.LENGTH_SHORT).show()
+
+                }
+                if (currentVehicle.contains("C5 Aircross")) {
+                    //binding.idTvCheckVehicle.text = "C5 Aircross"
+                    //Toast.makeText(activity, "" + currentVehicle, Toast.LENGTH_SHORT).show()
+                    getC5AirCrossInfo()
+                }
+            }
+
+        }
+
+        /*isNotification()*/
         getUser()
         getVehicleOwned()
 
@@ -155,7 +189,74 @@ class VehiclePageFragment : Fragment() {
                                     )
                                 }
                                 spinner.adapter = dataAdapter
+
+                                val sharedPref: SharedPreferences =
+                                    requireActivity().getSharedPreferences("FileName",
+                                        Context.MODE_PRIVATE
+                                    )
+                                val spinnerValue = sharedPref.getInt("spinnerChoice", -1)
+                                if (spinnerValue != -1) binding.idIncludeTop.idTileChoiceVehicle.idChooseVehicleSpinner.setSelection(
+                                    spinnerValue
+                                )
                             }
+
+                        } ?: throw Exception("discovery null response")
+                    }
+                }
+            } catch (e: Exception) {
+                Toast.makeText(activity, "Network Error", Toast.LENGTH_LONG).show()
+                // Discovery error
+                Log.e(ContentValues.TAG, "callDiscovery(): Error: $e")
+            }
+        }
+    }
+
+    private fun getRenegadeInfo() {
+        getComponentsStatus()
+        binding.idIncludeTop.tileUserAndNotifications.idBadgeNotification.visibility = View.INVISIBLE
+
+    }
+
+    private fun getC5AirCrossInfo() {
+        // MOCK type fuel
+        isNotification()
+
+    }
+
+    private fun getComponentsStatus() {
+
+        // TODO: TO BE COMPLETED
+
+        activity?.let { PmsRepository.initialize(it) }
+        lifecycleScope.launch {
+            try {
+                coroutineScope {
+                    launch {
+                        PmsRepository.getComponentsStatus()?.let { vehicleInfoNew ->
+
+                            val healthOfComponent = vehicleInfoNew.components?.map { it.health }
+                            /*binding.includeCardviewStatus.idTextViewStatus.text =
+                                healthOfComponent?.get(1)*/
+
+                            if (healthOfComponent!!.contains("1")) {
+                                Toast.makeText(activity, "1", Toast.LENGTH_SHORT).show()
+                            }
+                            else if (healthOfComponent.contains("2")) {
+                                Toast.makeText(activity, "2", Toast.LENGTH_SHORT).show()
+                            }
+                            else if (healthOfComponent.contains("3")) {
+                                Toast.makeText(activity, "3", Toast.LENGTH_SHORT).show()
+                            }
+                            //Toast.makeText(activity, "!! " + healthOfComponent, Toast.LENGTH_SHORT).show()
+
+                            //Toast.makeText(activity, "!! --> " + healthOfComponent?.get(0), Toast.LENGTH_SHORT).show()
+
+                            /*for (i in healthOfComponent!!.indices) {
+
+                                Toast.makeText(activity, "FOR --> " + healthOfComponent[i], Toast.LENGTH_SHORT).show()
+
+                            }*/
+
 
                         } ?: throw Exception("discovery null response")
                     }
